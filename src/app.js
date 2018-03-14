ae6c4685b410355a0ed169f0f46b0d4aba479eaf
mkdoh3 <mkdoherty3@GMAIL.COM>
2018-03-13 21:15:27 -0500

    components and routes framed

--- src/app.js
+++ src/app.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, Switch, Link, NavLink} from 'react-router-dom';
+
+import AppRouter from './routers/AppRouter';
 
 import 'normalize.css/normalize.css';
 import './styles/styles.scss';
 
 
-ReactDOM.render(<p>Boilerplate</p>, document.getElementById('app'))
+
+ReactDOM.render(<AppRouter />, document.getElementById('app'))
 
---
+++ src/components/AddExpensivePage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+const AddExpensePage = () => (
+  <div>add expense</div>
+)
+
+export default AddExpensePage;
