6f15a7f4de7875aca3ce42d4211ddfd0d01e2827
mkdoh3 <mkdoherty3@GMAIL.COM>
2018-03-29 15:10:14 -0500

    redux practice

--- src/playground/redux-intro.js
+++ src/playground/redux-intro.js
@@ -20,7 +20,7 @@ const resetCount = () => ({
   type: 'RESET',
 })
 
-const store = createStore((state = { count: 0 }, action) => {
+const countReducer = (state = { count: 0 }, action) => {
   switch(action.type) {
 
     case 'INCREMENT' : 
@@ -44,7 +44,9 @@ const store = createStore((state = { count: 0 }, action) => {
 
     default : return state;
   }
-});
+}
+
+const store = createStore(countReducer);
 
 // console.log(store.getState());
 
